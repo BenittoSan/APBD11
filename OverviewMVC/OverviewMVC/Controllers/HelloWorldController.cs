13e99d93396faea98fb32b8127b4d43634606300
Beniamin <s23358@pjwstk.edu.pl>
2024-06-22 16:29:47 +0200

    Added View and controller

---
+++ OverviewMVC/OverviewMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+using System.Text.Encodings.Web;
+
+namespace OverviewMVC.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+}
---
+++ OverviewMVC/OverviewMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OverviewMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
